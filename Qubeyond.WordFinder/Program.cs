d69513887d8849fcb7a098981ef869ea7c3bf06c
Franco Redoni <franco.redoni@gmail.com>
2023-11-08 01:43:48 -0300

    Program class cleaning

--- Qubeyond.WordFinder/Program.cs
+++ Qubeyond.WordFinder/Program.cs
@@ -1,35 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Qubeyond.WordFinder;
-
-var matrix = new List<string>
-{
-    { "adsgadsgadsfgadfghsfdghdfgjwh" },
-    { "asdgsdfhdfgnfghmghkjmsdfhdfoh" },
-    { "dfgasdgasdgasdfgsdfgsdfgsdfrg" },
-    { "aasdfgsdfhgdfghdfgjtukjiuyodi" },
-    { "gsgsdfghdfyhnmyimopopsdfhbdig" },
-    { "adgsdfhgsfhjuhoujoijgoijijfsf" },
-    { "uieooauiehjdlpukllusiueoekkfn" },
-    { "afjdglksjgiumabmiuuiasiouroro" },
-    { "bombastickhjalsjkdhgkjanrojgn" },
-    { "klajskljdfyeyunomkladjgklsdjf" },
-    { "kljsadflkhjsdflkhjhorafdagsdf" },
-    { "lknjolijouinlbjinlkuixhjouaae" }
-
-};
-
-var wordFinder = new WordFinder(matrix);
-
-var wordsToSearch = new List<string>
-{
-    { "bombastic" },
-    { "hora" },
-    { "gmh" },
-    { "frgjda" },
-    { "bmiuu" }
-};
-
-var wordsFound = wordFinder.Find(wordsToSearch);
-
-Console.WriteLine(wordsFound);
+Console.WriteLine("Hello world");
